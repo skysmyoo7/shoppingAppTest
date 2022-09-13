71b4adcb40795bf9fe6e8a9708d56405b54d2f84
skysmyoo7 <skysmyoo7@naver.com>
2022-09-13 16:12:58 +0900

    3-2. Home 화면과 Category 화면으로 분리

--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -1,4 +1,35 @@
 package screen
 
 class ShoppingCategory {
+
+    public fun showCategories() {
+        val category = arrayListOf<String>("전자기기", "패션", "반려동물물품")
+        for (i in category) {
+            println(i)
+        }
+        println("=> 장바구니로 이동하려면 #을 눌러주세요.")
+
+        var selectCategory = readLine()
+        while (selectCategory.isNullOrBlank() && selectCategory !in category) {
+            println("다시 입력해주세요.")
+            selectCategory = readLine()
+        }
+        if (selectCategory == "#") {
+            println("장바구니로 이동합니다.")
+        } else {
+
+            if(category.contains(selectCategory)){
+
+            }else{
+                showErrorMessage(selectCategory)
+            }
+
+        }
+    }
+
+    private fun showErrorMessage(selectCategory: String?) {
+        println("$selectCategory : 존재하지 않는 카테고리 입니다. 다시 입력해주세요. ")
+        showCategories()
+    }
+
 }
--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -14,23 +14,9 @@ class ShoppingHome {
         println("목록을 선택해주세요.")
     }
 
-    private fun showCategories() {
-        val category = arrayListOf<String>("전자기기", "패션", "반려동물물품")
-        for (i in category) {
-            println(i)
-        }
-        println("=> 장바구니로 이동하려면 #을 눌러주세요.")
-
-        var selectCategory = readLine()
-        while (selectCategory.isNullOrBlank() && selectCategory !in category) {
-            println("다시 입력해주세요.")
-            selectCategory = readLine()
-        }
-        if (selectCategory == "#") {
-            println("장바구니로 이동합니다.")
-        } else {
-
-        }
+    private fun showCategories(){
+        val shoppingCategory = ShoppingCategory()
+        shoppingCategory.showCategories()
     }
 
 
